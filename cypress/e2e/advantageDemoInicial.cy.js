a741e7e248495e510249dc0bee18071d661ecdac
fabioDev21 <fabiotwist1@gmail.com>
2025-04-23 11:21:59 -0300

    adicionando segundo teste

--- cypress/e2e/advantageDemoInicial.cy.js
+++ cypress/e2e/advantageDemoInicial.cy.js
@@ -24,4 +24,38 @@ describe('Pagina inicial Advantage Shopping', () => {
     cy.contains('TABLET')
 
   })
+  it(('Incluir produto no carrinho'), () => {
+
+    cy.get('#search > a')
+      .click()
+
+    cy.wait(5500) // Necessario para aguardar o carregamento da API da tela inicial
+
+    cy.get('#autoComplete')
+      .type('laptop')
+      .should('have.value','laptop')
+    
+    cy.get('[href="#/product/7"]')
+      .click()
+  
+    cy.url()
+      .should('contain','product')
+
+    cy.contains('LAPTOP')
+
+    cy.get('.fixedBtn > .roboto-medium')
+      .click()
+
+    cy.get('#shoppingCartLink')
+      .trigger('mouseover')
+
+    cy.contains('CHECKOUT')
+
+    cy.get('li > #toolTipCart > :nth-child(1) > table > tfoot > :nth-child(2) > td > #checkOutPopUp')
+      .click()
+
+    cy.contains('ORDER PAYMENT')
+  })
+
+
 })
