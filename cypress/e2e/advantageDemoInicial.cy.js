48f7d8884f7eba8abe1b82628753041ff7c84294
fabioDev21 <fabiotwist1@gmail.com>
2025-04-23 20:49:55 -0300

    adicionando terceiro teste

--- cypress/e2e/advantageDemoInicial.cy.js
+++ cypress/e2e/advantageDemoInicial.cy.js
@@ -29,7 +29,7 @@ describe('Pagina inicial Advantage Shopping', () => {
     cy.get('#search > a')
       .click()
 
-    cy.wait(5500) // Necessario para aguardar o carregamento da API da tela inicial
+    cy.wait(5000)
 
     cy.get('#autoComplete')
       .type('laptop')
@@ -49,13 +49,59 @@ describe('Pagina inicial Advantage Shopping', () => {
     cy.get('#shoppingCartLink')
       .trigger('mouseover')
 
-    cy.contains('CHECKOUT')
-
-    cy.get('li > #toolTipCart > :nth-child(1) > table > tfoot > :nth-child(2) > td > #checkOutPopUp')
+  })
+  it(('Validar os produtos incluidos no carrinho na tela de pagamento'), () => {
+    cy.get('#search > a')
       .click()
+
+    cy.wait(5000)
+
+    cy.get('#autoComplete')
+      .type('headphone')
+      .should('have.value','headphone')
+    
+    cy.contains('BEATS STUDIO 2 OVER-EAR MATTE BLACK HEADPHONES')
+      .click()
+
+    cy.wait(6000)
+
+    cy.contains('BEATS STUDIO 2 OVER-EAR MATTE BLACK HEADPHONES')
+
+    cy.url()
+      .should('contain','product')
+
+    cy.get('#lengthAllImages > :nth-child(2) > img')
+      .click()
+
+    // Necessario para guardar o atrbiuto src da imagem e usar para assercao depois da execucao
+
+    let srcSegundaImg
+    let srcImgCheckout
+
+    cy.get('#mainImg')
+      .invoke('attr', 'src')
+      .then((src) => {
+        srcSegundaImg = src
+      })
+
+    cy.get('.fixedBtn > .roboto-medium')
+      .click()
+
+    cy.get('#shoppingCartLink')
+      .trigger('mouseover')
+
+    cy.contains('CHECKOUT')
+      .click()
+    
+    cy.get('#userCart > #toolTipCart > :nth-child(1) > table > tbody > #product > :nth-child(1) > a > .imageUrl')
+      .invoke('attr', 'src')
+      .then((src) => {
+        srcImgCheckout = src
+      })
+    
+    expect(srcSegundaImg).to.equal(srcImgCheckout)
 
     cy.contains('ORDER PAYMENT')
   })
-
 
 })
