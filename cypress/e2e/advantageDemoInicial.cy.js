2091bac7f6f66c15cb82722520638296e1aa101e
fabioDev21 <fabiotwist1@gmail.com>
2025-04-23 08:42:22 -0300

    add teste 'Realize a busca de um produto' como primeiro teste

--- cypress/e2e/advantageDemoInicial.cy.js
+++ cypress/e2e/advantageDemoInicial.cy.js
@@ -9,8 +9,19 @@ describe('Pagina inicial Advantage Shopping', () => {
     cy.get('#search > a')
       .click()
 
+    cy.wait(5500) // Necessario para aguardar o carregamento da API da tela inicial
+
     cy.get('#autoComplete')
-      .type("tablet", {force: true})
+      .type('tablet')
+      .should('have.value','tablet')
     
+    cy.get('.top6Products > [href="#/product/16"]')
+      .click()
+  
+    cy.url()
+      .should('contain','product')
+
+    cy.contains('TABLET')
+
   })
 })
